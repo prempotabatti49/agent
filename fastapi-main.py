8584d9538d3452c5c6e4486820fd1ff475da950f
Prem Potabatti <prempx2020@email.iimcal.ac.in>
2025-09-26 11:34:00 +0530

    added fastapi

--- fastapi-main.py
+++ fastapi-main.py
@@ -2,17 +2,32 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from models.models import ChatRequest, print_something
+
+print_something()
+
+app = FastAPI()
+
 # Load environment variables from .env file
 load_dotenv()
 
 client = OpenAI()  # or leave empty if set in env
 
-response = client.chat.completions.create(
-    model="gpt-4o-mini",  # cheaper + faster model for apps
-    messages=[
-        {"role": "system", "content": "You are a helpful AI assistant."},
-        {"role": "user", "content": "Write a very seductive and sexy conversation between a man and a girl in less than 8 sentences"}
-    ]
-)
+@app.post("/chat")
+def chat(request: ChatRequest):
+    response = client.chat.completions.create(
+        model="gpt-4o",  # cheaper + faster model for apps
+        messages=[
+            {"role": "system", "content": "You are a helpful AI assistant."},
+            {"role": "user", "content": f"{request.message}"}
+        ]   
+    )
+    x = response.choices[0].message.content.strip().replace("\n", "<br>")
+    print(response.choices[0].message.content)
+    return {"reply": x}
 
-print(response.choices[0].message.content)
+
+
