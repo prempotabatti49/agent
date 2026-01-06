e37566b9188d7b2500604fea88d947cbc82c61d4
Prem Potabatti <prempx2020@email.iimcal.ac.in>
2026-01-06 16:13:12 +0530

    adding secrets in task definition and remvoing load_env in main.py

--- main.py
+++ main.py
@@ -12,40 +12,40 @@ from botocore.exceptions import ClientError
 import json
 
 print(f"START OF MAIN FUNCTION")
-def get_secret():
-    secret_name = "openai_api_key"
-    region_name = "us-east-1"
+# def get_secret():
+#     secret_name = "openai_api_key"
+#     region_name = "us-east-1"
 
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
+#     # Create a Secrets Manager client
+#     session = boto3.session.Session()
+#     client = session.client(
+#         service_name='secretsmanager',
+#         region_name=region_name
+#     )
 
-    try:
-        response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
+#     try:
+#         response = client.get_secret_value(
+#             SecretId=secret_name
+#         )
+#     except ClientError as e:
+#         # For a list of exceptions thrown, see
+#         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+#         raise e
 
-    secret = json.loads(response['SecretString'])
-    return secret['OPENAI_API_KEY']
+#     secret = json.loads(response['SecretString'])
+#     return secret['OPENAI_API_KEY']
 
-print("READING OPEN AI SECRET")
-openai_api_key = get_secret()
-print('SETTING OPENAI KEY IN ENV VARIABLES')
-os.environ["OPENAI_API_KEY"] = openai_api_key
+# print("READING OPEN AI SECRET")
+# openai_api_key = get_secret()
+# print('SETTING OPENAI KEY IN ENV VARIABLES')
+# os.environ["OPENAI_API_KEY"] = openai_api_key
 
 print("STARTING FASTAPI")
 app = FastAPI()
 
 # Load environment variables from .env file
-print("LOADING ENV VARIABLES")
-load_dotenv()
+# print("LOADING ENV VARIABLES")
+# load_dotenv()
 print('OPENAI - INSTANTIATION')
 client = OpenAI()
 
